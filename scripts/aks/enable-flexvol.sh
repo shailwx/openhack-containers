5e148f3a2eba1c76167897dec05d30f2b714ed26
Johan Dahlbom <jodahlbo@microsoft.com>
2020-01-22 15:33:53 +0300

    flexvol update

--- scripts/aks/enable-flexvol.sh
+++ scripts/aks/enable-flexvol.sh
@@ -2,11 +2,11 @@
 kubectl apply -f https://raw.githubusercontent.com/Azure/kubernetes-keyvault-flexvol/master/deployment/kv-flexvol-psp.yaml
 
 #User managed identity resource id and key vault (you don't need to change since we are sharing)
-$umi="/subscriptions/67db0cb5-d026-4e61-9c32-5d98c5df9058/resourcegroups/teamResources/providers/Microsoft.ManagedIdentity/userAssignedIdentities/k8s-flexvol"
-$keyvault='WA8KSKeyVault'
+umi="/subscriptions/67db0cb5-d026-4e61-9c32-5d98c5df9058/resourcegroups/teamResources/providers/Microsoft.ManagedIdentity/userAssignedIdentities/k8s-flexvol"
+keyvault='WA8KSKeyVault'
 #Enable the user managed identity in cluster 
-$noderesourcegroup='MC_jdk8s_jdk8saad_northeurope'
-$nodepoolvmss='aks-nodepool1-19503395-vmss'
+noderesourcegroup='MC_jdk8s_jdk8saad_northeurope'
+nodepoolvmss='aks-nodepool1-19503395-vmss'
 
 
 #Assign identity to the vmss 
